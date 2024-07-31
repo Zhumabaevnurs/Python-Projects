0e986e82c5ae87cef671ae9c259c5547abb6534c
Zhumabaevnurs <42982243+Zhumabaevnurs@users.noreply.github.com>
2024-07-31 18:38:24 +0000

    Initial commit

---
+++ README.mk
@@ -0,0 +1,27 @@
+Caesar Cipher
+This is a simple Caesar Cipher program implemented in Python. It allows you to encode and decode messages by shifting the letters of the alphabet.
+
+Features:
+Encode a message
+Decode a message
+Supports shift values greater than 26
+
+
+Prerequisites:
+Python 3.x
+
+How to Run
+1.Ensure you have Python 3.x installed on your system.
+2.Copy the code into a file named caesar_cipher.py.
+3.Create an art.py file containing the logo variable with your desired ASCII art.
+4.Run the caesar_cipher.py script using Python:
+
+Usage:
+1.Run the script.
+2.Enter 'encode' to encrypt or 'decode' to decrypt a message.
+3.Enter your message.
+4.Enter the shift number.
+5.The encoded or decoded message will be displayed.
+6.Type 'yes' to run the cipher again or 'no' to exit the program.
+
+Enjoy using the Caesar Cipher program!
